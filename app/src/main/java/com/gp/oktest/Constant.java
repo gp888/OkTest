a449f608cd4420a013368fdba8ac9f925aa2917a
gp_dbn_nxin <18832868180@163.com>
2019-02-22 11:00:56 +0800

    crashHadler

--- app/src/main/java/com/gp/oktest/Constant.java
+++ app/src/main/java/com/gp/oktest/Constant.java
@@ -12,6 +12,7 @@ public class Constant {
     public static File mExternalStorage = Environment.getExternalStorageDirectory();
     public static final String DIR_IMAGE_TEMP = "/okTest/temp/"; //临时目录
     public static final String IMAGE_TEMP_PATH = mExternalStorage + Constant.DIR_IMAGE_TEMP;  //图片保存路径
+    public static final String CRASH_PATH = mExternalStorage + "/okTest/crash/";
 
     public static final String FILE_PROVIDER_AUTHORITY = "com.gp.oktest.fileprovider";
 
