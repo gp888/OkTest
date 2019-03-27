793c8111d49b170e9b62387bf5d8a7bec976dc37
gp888 <ping651803325@hotmail.com>
2019-03-27 23:05:11 +0800

    java basic

--- app/src/main/java/com/gp/oktest/javatest/JavaTest.java
+++ app/src/main/java/com/gp/oktest/javatest/JavaTest.java
@@ -6,5 +6,37 @@ public class JavaTest {
     public static void main(String[] args) {
         String str = new String("乳猪浓缩" + "\n" +"料乳猪浓" + "\n" + "缩料");
         System.out.print("heheh" + str);
+
+        //是否是素数
+        double m = 1000;
+        double k = Math.sqrt(m);
+        for(int i = 2; i <= k; i++) {
+            if (m % i == 0) {
+                System.out.print(i + "");
+                break;
+            }
+        }
+
+        //冒泡排序
+        int [] a = {5, 6, 7, 8, 9};
+        for(int j = 0; j < a.length; j ++) {
+            for(int i = 0; i < a.length - j; i++) {
+                if(a[i] > a[i + 1]) {
+                    int t = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = t;
+                }
+            }
+        }
+
+        //斐波那契
+        int f1 = 1, f2 = 1;
+        for(int i = 3; i < 20; i++) {
+            f1 = f1 + f2;
+            f2 = f1 + f2;
+        }
+
+
+
     }
 }
