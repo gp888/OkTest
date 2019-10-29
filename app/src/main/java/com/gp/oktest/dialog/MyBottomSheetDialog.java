31b141a95b7f5055dae20850e0ee426a3a2e767a
guoping <guoping@17guagua.com>
2019-10-29 19:59:00 +0800

    BottomSheetDialog和dialogFragment

---
+++ app/src/main/java/com/gp/oktest/dialog/MyBottomSheetDialog.java
@@ -0,0 +1,131 @@
+package com.gp.oktest.dialog;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+
+public class MyBottomSheetDialog extends BottomSheetDialog {
+
+    private int mPeekHeight;
+    private int mMaxHeight;
+    private boolean mCreated;
+    private Window mWindow;
+    private BottomSheetBehavior mBottomSheetBehavior;
+
+    public MyBottomSheetDialog(@NonNull Context context, int peekHeight, int maxHeight) {
+        super(context);
+        init(peekHeight, maxHeight);
+    }
+
+    public MyBottomSheetDialog(@NonNull Context context, int theme,int peekHeight,int maxHeight) {
+        super(context, theme);
+        init(peekHeight, maxHeight);
+    }
+
+    public MyBottomSheetDialog(@NonNull Context context, boolean cancelable, OnCancelListener cancelListener,int peekHeight,int maxHeight) {
+        super(context, cancelable, cancelListener);
+        init(peekHeight, maxHeight);
+    }
+
+    private void init(int peekHeight, int maxHeight) {
+        mWindow = getWindow();
+        mPeekHeight = peekHeight;
+        mMaxHeight = maxHeight;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mCreated = true;
+
+        setPeekHeight();
+        setMaxHeight();
+        setBottomSheetCallback();
+    }
+
+    public void setPeekHeight(int peekHeight) {
+        mPeekHeight = peekHeight;
+
+        if (mCreated) {
+            setPeekHeight();
+        }
+    }
+
+    public void setMaxHeight(int height) {
+        mMaxHeight = height;
+
+        if (mCreated) {
+            setMaxHeight();
+        }
+    }
+
+    public void setBatterSwipeDismiss(boolean enabled) {
+        if (enabled) {
+
+        }
+    }
+
+    private void setPeekHeight() {
+        if (mPeekHeight <= 0) {
+            return;
+        }
+
+        if (getBottomSheetBehavior() != null) {
+            mBottomSheetBehavior.setPeekHeight(mPeekHeight);
+        }
+    }
+
+    private void setMaxHeight() {
+        if (mMaxHeight <= 0) {
+            return;
+        }
+
+        mWindow.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, mMaxHeight);
+        mWindow.setGravity(Gravity.BOTTOM);
+    }
+
+    private BottomSheetBehavior getBottomSheetBehavior() {
+        if (mBottomSheetBehavior != null) {
+            return mBottomSheetBehavior;
+        }
+
+        View view = mWindow.findViewById(android.support.design.R.id.design_bottom_sheet);
+        // setContentView() 没有调用
+        if (view == null) {
+            return null;
+        }
+        mBottomSheetBehavior = BottomSheetBehavior.from(view);
+        return mBottomSheetBehavior;
+    }
+
+    private void setBottomSheetCallback() {
+        if (getBottomSheetBehavior() != null) {
+            mBottomSheetBehavior.setBottomSheetCallback(mBottomSheetCallback);
+        }
+    }
+
+    private final BottomSheetBehavior.BottomSheetCallback mBottomSheetCallback
+            = new BottomSheetBehavior.BottomSheetCallback() {
+        @Override
+        public void onStateChanged(@NonNull View bottomSheet,
+                                   @BottomSheetBehavior.State int newState) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                dismiss();
+                BottomSheetBehavior.from(bottomSheet).setState(
+                        BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        }
+
+        @Override
+        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+        }
+    };
+}
--- app/src/main/java/com/gp/oktest/fragment/MyDialogFragment.java
+++ app/src/main/java/com/gp/oktest/fragment/MyDialogFragment.java
@@ -3,13 +3,85 @@ package com.gp.oktest.fragment;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatDialog;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
+/**
+ * MyDialogFragment dialogFragment = new MyDialogFragment();
+ *         dialogFragment.show(getSupportFragmentManager(), "tag");
+ *         单个实例只能show一次
+ */
 public class MyDialogFragment extends DialogFragment {
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
+        AppCompatDialog appCompatDialog = new AppCompatDialog(requireActivity());
+        TextView textView = new TextView(requireActivity());
+        textView.setText("通过onCreateDialog使用DialogFragment");
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 28);
+        appCompatDialog.setContentView(textView);
+        return appCompatDialog;
+    }
+
+    /**
+     * 创建的View将会作为Dialog的内容布局,优先级高于@onCreateDialog
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView textView = new TextView(requireActivity());
+        textView.setText("通过onCreateView使用DialogFragment");
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 28);
+        return textView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //设置Dialog的样式
+        //必须在onCretaeDialog方法后面执行，不然得不到Dialog实例
+
+        Dialog dialog = getDialog();
+        Window window = dialog.getWindow();
+        if (window == null) {
+            return;
+        }
+        WindowManager.LayoutParams attributes = window.getAttributes();
+        //设置Dialog窗口的高度
+        attributes.height = WindowManager.LayoutParams.MATCH_PARENT;
+        //设置Dialog窗口的宽度
+        attributes.width = WindowManager.LayoutParams.MATCH_PARENT;
+        //设置Dialog的居中方向
+        attributes.gravity = Gravity.CENTER;
+        //设置Dialog弹出时背景的透明度
+        attributes.dimAmount = 0.6f;
+        //设置Dialog水平方向的间距
+        attributes.horizontalMargin = 0f;
+        //设置Dialog垂直方向的间距
+        attributes.verticalMargin = 0f;
+        //设置Dialog显示时X轴的坐标,具体屏幕X轴的偏移量
+        attributes.x = 0;
+        //设置Dialog显示时Y轴的坐标,距离屏幕Y轴的偏移量
+        attributes.y = 0;
+        //设置Dialog的透明度
+        attributes.alpha = 0f;
+        //设置Dialog显示和消失时的动画
+        attributes.windowAnimations = 0;
+        window.setAttributes(attributes);
     }
 }
